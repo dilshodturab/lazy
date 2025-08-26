a4d59ccaed94357e8b683bdc39d6132f739aeccf
dilshodturab <avlomberdioglid@gmail.com>
2025-08-26 08:43:39 +0500

    explorer postion modified to my will

---
+++ lua/plugins/explorer.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          layout = {
+            layout = {
+              position = "right", -- Change to "left", "top", "bottom" as desired
+            },
+          },
+          -- Other explorer options...
+        },
+      },
+    },
+    -- Other snacks options...
+  },
+}
